c2e6f9234fda31a0d7941be043273eb4a73d4b87
Rynilan <ilanvitorsantos@gmail.com>
2024-11-30 15:32:56 -0300

    code: criada a tela inicial e arquivo principal (inicial).'
    Signed-off-by: Rynilan <ilanvitorsantos@gmail.com>

--- principal.py
+++ principal.py
@@ -1,2 +1,35 @@
-from crud import *
+from tkinter import Tk, Label, Button, CENTER, mainloop
 
+
+def destruir():
+    global visualizar, gerenciar
+    visualizar.destroy()
+    gerenciar.destroy()
+
+
+def gerir():
+    from view.código_tela_de_adicionamento import Tela
+    destruir()
+    Tela()
+
+
+def ver():
+    from view.personagens import personagens
+    destruir()
+    personagem()
+
+
+if __name__ == '__main__':
+    master = Tk()
+    master.geometry('720x480')
+
+    titulo = Label(master, text='Pokedex')
+    titulo.pack()
+
+    gerenciar = Button(master, text='Tela de CRUD', command=None)
+    gerenciar.place(relx=0.3, rely=0.5, anchor=CENTER)
+
+    visualizar = Button(master, text='Ver pokemons', command=None)
+    visualizar.place(relx=0.7, rely=0.5, anchor=CENTER)
+
+    mainloop()
